059c51543a9880fd4feb59b7bbbe9f150a2dad30
Rob Jenks <robjenks@gmail.com>
2023-10-30 17:44:42 -0400

    Update coverage script

--- codegen-service/scripts/get-test-coverage.sh
+++ codegen-service/scripts/get-test-coverage.sh
@@ -1,1 +1,9 @@
-awk -F"," '{ instructions += $4 + $5; covered += $5 } END { print covered, "/", instructions, " instructions covered"; print 100*covered/instructions, "% covered" }' ../jacoco/target/site/jacoco-aggregate/jacoco.csv
+JACOCO_MODULE=$1
+
+if [ -z $JACOCO_MODULE ]
+then
+  echo "Provide path to Jacoco module as script argument"
+  exit 1
+fi
+
+awk -F"," '{ instructions += $4 + $5; covered += $5 } END { print covered, "/", instructions, " instructions covered"; print 100*covered/instructions, "% covered" }' "${JACOCO_MODULE}"/target/site/jacoco-aggregate/jacoco.csv
