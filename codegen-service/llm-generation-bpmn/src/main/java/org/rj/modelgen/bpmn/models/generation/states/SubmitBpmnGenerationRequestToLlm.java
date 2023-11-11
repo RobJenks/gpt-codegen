e0fb5877d4a9bd43b35abfe0cc0b1f7db036349d
Rob Jenks <robjenks@gmail.com>
2023-11-11 17:56:04 -0500

    Support custom http options in llm submission

--- codegen-service/llm-generation-bpmn/src/main/java/org/rj/modelgen/bpmn/models/generation/states/SubmitBpmnGenerationRequestToLlm.java
+++ codegen-service/llm-generation-bpmn/src/main/java/org/rj/modelgen/bpmn/models/generation/states/SubmitBpmnGenerationRequestToLlm.java
@@ -5,6 +5,7 @@ import org.rj.modelgen.bpmn.exception.BpmnGenerationException;
 import org.rj.modelgen.bpmn.models.generation.signals.LlmResponseReceived;
 import org.rj.modelgen.bpmn.models.generation.signals.LlmModelRequestPreparedSuccessfully;
 import org.rj.modelgen.llm.request.ModelRequest;
+import org.rj.modelgen.llm.request.ModelRequestHttpOptions;
 import org.rj.modelgen.llm.response.ModelResponse;
 import org.rj.modelgen.llm.state.ModelInterfaceSignal;
 import org.rj.modelgen.llm.state.ModelInterfaceState;
@@ -32,10 +33,14 @@ public class SubmitBpmnGenerationRequestToLlm extends ModelInterfaceState<LlmMod
 
         final var request = new ModelRequest("gpt-4", 0.7f, input.getContext());
         return getModelInterface().createSessionIfRequired(input.getSessionId())
-                .flatMap(__ -> getModelInterface().submit(input.getSessionId(), request))
+                .flatMap(__ -> getModelInterface().submit(input.getSessionId(), request, getHttpOptions()))
                 .map(response -> tuple(response, sanitizer.sanitize(response.getMessage())))
                 .doOnSuccess(responseAndSanitizedContent -> recordModelResponse(input.getSessionId(), responseAndSanitizedContent.v1, responseAndSanitizedContent.v2))
                 .flatMap(responseAndSanitizedContent -> outboundSignal(new LlmResponseReceived(input.getSessionId(), responseAndSanitizedContent.v1, responseAndSanitizedContent.v2)));
+    }
+
+    protected ModelRequestHttpOptions getHttpOptions() {
+        return new ModelRequestHttpOptions();
     }
 
     private void recordModelResponse(String sessionId, ModelResponse modelResponse, String sanitizedContent) {
