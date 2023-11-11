dc9d36736396f632e72994b9d4c12e51549df3ae
Rob Jenks <robjenks@gmail.com>
2023-11-11 17:11:42 -0500

    Enable metadata on signals

--- codegen-service/llm-generation-core/src/main/java/org/rj/modelgen/llm/state/ModelInterfaceSignal.java
+++ codegen-service/llm-generation-core/src/main/java/org/rj/modelgen/llm/state/ModelInterfaceSignal.java
@@ -63,7 +63,7 @@ public abstract class ModelInterfaceSignal {
     }
 
     public void addMetadata(String key, Object data) {
-        this.metadata.put(key, data)
+        this.metadata.put(key, data);
     }
 
     public void setMetadata(Map<String, Object> metadata) {
