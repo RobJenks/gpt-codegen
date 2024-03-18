9bcb6fae0262dde249a057743ab1a739278ce520
Rob Jenks <robjenks@gmail.com>
2024-03-18 14:39:40 -0400

    Move prompt placeholders to common module

--- codegen-service/llm-generation-bpmn/src/main/java/org/rj/modelgen/bpmn/models/generation/context/BpmnGenerationPromptPlaceholders.java
+++
@@ -1,9 +0,0 @@
-package org.rj.modelgen.bpmn.models.generation.context;
-
-import org.rj.modelgen.llm.prompt.PromptPlaceholder;
-
-public class BpmnGenerationPromptPlaceholders {
-    public static final PromptPlaceholder SCHEMA_CONTENT = new PromptPlaceholder("SCHEMA_CONTENT");
-    public static final PromptPlaceholder CURRENT_STATE = new PromptPlaceholder("CURRENT_STATE");
-    public static final PromptPlaceholder PROMPT = new PromptPlaceholder("PROMPT");
-}
