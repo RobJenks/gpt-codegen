447b21f9051dedcc6f4482da38a0d9ff9b747211
Rob Jenks <robjenks@gmail.com>
2024-03-27 14:50:08 -0400

    Add error properties to standard model states

---
+++ codegen-service/llm-generation-core/src/main/java/org/rj/modelgen/llm/state/ModelInterfaceExecutionResultSummary.java
@@ -0,0 +1,10 @@
+package org.rj.modelgen.llm.state;
+
+/**
+ * Summary of a `ModelInterfaceExecutionResult` including the final result and the audit path to reach it
+ * Includes a subset of data from the execution result and excludes most low-level and internal detail
+ */
+public class ModelInterfaceExecutionResultSummary {
+    private boolean successful;
+
+}
