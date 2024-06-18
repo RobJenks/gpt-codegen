efc0269e5b7b0cf711abd95af6ecb8c34b886446
Rob Jenks <robjenks@gmail.com>
2024-06-18 13:11:36 -0400

    Allow extraneous fields in BPMN intermediate model

--- codegen-service/llm-generation-bpmn/src/main/java/org/rj/modelgen/bpmn/intrep/model/BpmnIntermediateModel.java
+++ codegen-service/llm-generation-bpmn/src/main/java/org/rj/modelgen/bpmn/intrep/model/BpmnIntermediateModel.java
@@ -1,8 +1,10 @@
 package org.rj.modelgen.bpmn.intrep.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.rj.modelgen.llm.intrep.graph.IntermediateGraphModel;
 
 /**
  * Implementation of graph-specialized IR for BPMN models
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BpmnIntermediateModel extends IntermediateGraphModel<String, ElementConnection, ElementNode> { }
